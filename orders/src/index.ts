29c33219338d985135f934bf293d84d7a81a3494
Matan <ml.git201@gmail.com>
2024-12-23 22:09:35 +0200

    feat(orders): added ticket created/updated event listeners

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -3,6 +3,9 @@ import mongoose from "mongoose";
 import { app } from "./app";
 import { queueWrapper } from "./queue-wrapper";
 
+import { TicketCreatedListener } from "./events/listeners/ticket-created-listener";
+import { TicketUpdatedListener } from "./events/listeners/ticket-updated-listener";
+
 const start = async () => {
   if (!process.env.JWT_KEY) {
     throw new Error("JWT_KEY must be defined");
@@ -32,6 +35,8 @@ const start = async () => {
     });
     process.on("SIGINT", () => queueWrapper.client.close());
     process.on("SIGTERM", () => queueWrapper.client.close());
+    new TicketCreatedListener(queueWrapper.client).listen();
+    new TicketUpdatedListener(queueWrapper.client).listen();
     await mongoose.connect(process.env.DB_URI);
     console.log("Connected to MongoDb");
   } catch (err) {
